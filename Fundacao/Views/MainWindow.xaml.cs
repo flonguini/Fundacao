eb584bd46c0dadf10dd5d8d83e8bd3ceb9a06ea1
Fernando Longuini <flonguini@outlook.com>
2018-07-29 13:43:21 -0300

    Criação da View SapataIsolada

--- Fundacao/Views/MainWindow.xaml.cs
+++ Fundacao/Views/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Fundacao.Views;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Fundacao
 {
@@ -23,6 +11,7 @@ namespace Fundacao
         public MainWindow()
         {
             InitializeComponent();
+            MainContentControl.Content = new SapataIsoladaView();
         }
     }
 }
